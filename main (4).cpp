3b39729a78f809fb0906c34ec9245c3aa3344aaa
Vinayak Sharma <77274104+Tomhawkstorm55557@users.noreply.github.com>
2023-09-14 23:59:40 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int a;
+        int b;
+        cin>>a>>b;
+        int ans =0;
+        cout<< min({a,b,(a+b)/3})<<endl;
+       
+
+        }
+          return 0;
+    }
+
+
+  
+
